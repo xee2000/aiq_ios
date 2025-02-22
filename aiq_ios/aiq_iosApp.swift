25bd2492ce61c638d957bee5cab2b459d8463758
이정호 <xee2000@naver.com>
2025-02-22 21:36:20 +0900

    Initial Commit

---
+++ aiq_ios/aiq_iosApp.swift
@@ -0,0 +1,32 @@
+//
+//  aiq_iosApp.swift
+//  aiq_ios
+//
+//  Created by 이정호 on 2/22/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct aiq_iosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ aiq_iosTests/aiq_iosTests.swift
@@ -0,0 +1,16 @@
+//
+//  aiq_iosTests.swift
+//  aiq_iosTests
+//
+//  Created by 이정호 on 2/22/25.
+//
+
+import Testing
+
+struct aiq_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
