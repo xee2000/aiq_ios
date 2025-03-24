b5fd0beab24d0421b7bb4c8bb9bb912e5a0302d5
이정호 <xee2000@naver.com>
2025-03-24 17:49:37 +0900

    코드 수정진행 // 김의수부장님 코드와 동일하게 맞추도록 변동처리

---
+++ aiq_ios/domain/UserNotificationType.swift
@@ -0,0 +1,13 @@
+//
+//  UserNotificationType.swift
+//  cordoba_aiq
+//
+//  Created by Sun,Kim on 2/19/25.
+//
+
+enum UserNotificationType: String {
+    case Init
+    case Onepass
+    case Parking
+    case Doorphone
+}
---
+++ aiq_ios/function/DebugLog.swift
@@ -0,0 +1,15 @@
+//
+//  DebugLog.swift
+//  cordoba_aiq
+//
+//  Created by Sun,Kim on 2/28/25.
+//  print log를 DEBUG Mode에서만 출력하기 위한 함수임.
+//
+
+class DebugLog: NSObject {
+  static func log(_ tag: String, items: Any..., separator: String = " ", terminator: String = "\n") {
+#if DEBUG
+    Swift.print("\(Date()) \(tag)", items.map {"\($0)"}.joined(separator: separator), terminator: terminator)
+#endif
+  }
+}
